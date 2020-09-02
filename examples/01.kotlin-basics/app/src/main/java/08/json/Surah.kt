4ff4aaa10114108f4158ecbfcdb1d61165a6876e
Abdelkarim Erradi <erradi@live.com>
2020-09-03 00:19:34 +0300

    Improved json example

--- examples/01.kotlin-basics/app/src/main/java/08/json/Surah.kt
+++ examples/01.kotlin-basics/app/src/main/java/08/json/Surah.kt
@@ -5,18 +5,16 @@ import kotlinx.serialization.Serializable
 /*
 To be able use @Serializable and Json class you need to add these dependencies then sync:
 1) Add to dependencies of the 1st build.gradle:
-        //Added for Kotlin Serialization
-        classpath "org.jetbrains.kotlin:kotlin-serialization:$kotlin_version"
+    //Added for Kotlin Serialization
+    classpath "org.jetbrains.kotlin:kotlin-serialization:$kotlin_version"
 
 2) Add to dependencies of the 2nd build.gradle
     //Added for Kotlin Serialization
-    implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version" // or "kotlin-stdlib-jdk8"
-    implementation "org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC" // JVM dependency
+    implementation "org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC"
 
 3) Add this apply plugin to the 2nd build.gradle before line "android {"
-//Added for Kotlin Serialization
-apply plugin: 'kotlinx-serialization'
-
+    //Added for Kotlin Serialization
+    apply plugin: 'kotlinx-serialization'
  */
 
 @Serializable
