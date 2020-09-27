b9a22c8ac368fd2c90342ccfd80043b26100021f
Abdelkarim Erradi <erradi@live.com>
2020-09-27 16:23:29 +0300

    MVVM Updated slides and examples

--- examples/07.mvvm/MVVMSample/app/src/main/java/qa/edu/cmps312/mvvm/MainActivity.kt
+++ examples/07.mvvm/MVVMSample/app/src/main/java/qa/edu/cmps312/mvvm/MainActivity.kt
@@ -1,21 +1,40 @@
 package qa.edu.cmps312.mvvm
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import qa.edu.cmps312.mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+        //setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
 
-        // Associate the Activity with the ViewModel
+        // Connect the binding in the Activity with the  ViewModel
         val viewModel by viewModels<MainActivityViewModel>()
         //Or ViewModelProvider(<this activity>).get(<Your ViewModel>.class)
         //val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-        team1ScoreTv.text = viewModel.team1Score.toString()
+        // Assign data binding with the viewModel
+        // Binding to LiveData triggers UI updates when the data changes
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        closeAppBtn.setOnClickListener {
+            // Finish the activity - activity will be destroyed and the app will close
+            finish()
+        }
+
+        //team1ScoreTv.text = viewModel.team1Score.toString()
         team1IncrementBtn.setOnClickListener {
             viewModel.incrementTeam1Score()
             //team1ScoreTv.text = viewModel.team1Score.toString()
@@ -27,12 +46,15 @@ class MainActivity : AppCompatActivity() {
             //team2ScoreTv.text = viewModel.team2Score.toString()
         }
 
+
         viewModel.team1Score.observe(this, {
-            team1ScoreTv.text = it.toString()
+            Log.d("team1Score.observe", it.toString())
+            //team1ScoreTv.text = it.toString()
         })
 
         viewModel.team2Score.observe(this, {
-            team2ScoreTv.text = it.toString()
+            //team2ScoreTv.text = it.toString()
+            Log.d("team2Score.observe", it.toString())
         })
 
         /*
@@ -48,5 +70,35 @@ class MainActivity : AppCompatActivity() {
             team2ScoreTv.text = team2Score.toString()
         }
       */
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy. Is Finishing: $isFinishing")
     }
 }
--- examples/07.mvvm/MVVMSample/app/src/main/java/qa/edu/cmps312/mvvm/MainActivityViewModel.kt
+++ examples/07.mvvm/MVVMSample/app/src/main/java/qa/edu/cmps312/mvvm/MainActivityViewModel.kt
@@ -1,14 +1,23 @@
 package qa.edu.cmps312.mvvm
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
-    val team1Score = MutableLiveData<Int>(0)
-    val team2Score = MutableLiveData<Int>(0)
+    private val _team1Score = MutableLiveData<Int>(0)
+    private val _team2Score = MutableLiveData<Int>(0)
 
-    fun incrementTeam1Score() = team1Score.postValue(team1Score.value?.inc())
-    fun incrementTeam2Score() = team2Score.postValue(team2Score.value?.inc())
+    val team1Score: LiveData<Int> get() = _team1Score
+    val team2Score: LiveData<Int> get() = _team2Score
+
+    fun incrementTeam1Score() {
+        _team1Score.value = _team1Score.value?.inc()
+    }
+    fun incrementTeam2Score() {
+        _team2Score.value = _team2Score.value?.inc()
+    }
     /*
     var team1Score = 0
     var team2Score = 0
@@ -16,4 +25,9 @@ class MainActivityViewModel : ViewModel() {
     fun incrementTeam1Score() = team1Score++
     fun incrementTeam2Score() = team2Score++
    */
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("MainActivityViewModel", "onCleared")
+    }
 }
