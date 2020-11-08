a3782f6c9a8b6e20ae2fab847daeb353a68df459
Abdelkarim Erradi <erradi@live.com>
2020-11-08 23:22:03 +0300

    WorkManager updated slides and examples

--- examples/12.work-manager/app/src/main/java/qu/cmps312/workmanager/AppKeys.kt
+++ examples/12.work-manager/app/src/main/java/qu/cmps312/workmanager/AppKeys.kt
@@ -3,4 +3,5 @@ package qu.cmps312.workmanager
 object AppKeys {
     const val COUNT_VALUE = "key_count"
     const val CURRENT_DATE = "current_date"
+    const val PROGRESS = "Progress"
 }
--- examples/12.work-manager/app/src/main/java/qu/cmps312/workmanager/MainActivity.kt
+++ examples/12.work-manager/app/src/main/java/qu/cmps312/workmanager/MainActivity.kt
@@ -1,6 +1,7 @@
 package qu.cmps312.workmanager
 
 import android.app.Application
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -52,27 +53,7 @@ class MainActivity : AppCompatActivity() {
                    .then(uploadRequest)
                    .enqueue()*/
 
-        WorkManager.getInstance(applicationContext).enqueue(uploadRequest)
-
-        /*
-        WorkManager.getInstance(applicationContext).getWorkInfosByTag("Sync")
-        WorkManager.getInstance(applicationContext).getWorkInfosForUniqueWork("MyUniqueName")
-        WorkManager.getInstance(applicationContext).getWorkInfoById(uploadRequest.id)
-        WorkManager.getInstance(applicationContext).getWorkInfosByTag(TAG_SAVE)
-        WorkManager.getInstance(applicationContext).getWorkInfosByTagLiveData(TAG_SAVE)
-
-        WorkManager.getInstance(applicationContext).cancelAllWork()
-        */
-
-        //Source: developer.android.com/topic/libraries/architecture/workmanager/how-to/cancel-stop-work
-        /*
-        val saveImageWorkRequest = OneTimeWorkRequestBuilder<SaveImageWorker>()
-                .addTag(TAG_SAVE)
-                .build()
-
-        WorkManager.getInstance(applicationContext).cancelWorkById(saveImageWorkRequest.id)
-        WorkManager.getInstance(applicationContext).cancelAllWorkByTag(TAG_SAVE) */
-
+        workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
                    .observe(this, Observer {
                         textView.text = it.state.name
@@ -82,6 +63,19 @@ class MainActivity : AppCompatActivity() {
                             Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
                         }
                    })
+
+
+        // Observing Worker Progress
+        val request = OneTimeWorkRequestBuilder<ProgressWorker>().build()
+        WorkManager.getInstance(this)
+                .getWorkInfoByIdLiveData(request.id)
+                .observe(this, Observer { workInfo: WorkInfo? ->
+                    if (workInfo != null) {
+                        val progress = workInfo.progress
+                        val value = progress.getInt(AppKeys.PROGRESS, 0)
+                            // Do something with progress information
+                    }
+                })
     }
 
     private fun setPeriodicWorkRequest() {
@@ -101,8 +95,21 @@ class MainActivity : AppCompatActivity() {
                 .build()
     }
 
-
 }
+
+// Reporting Worker Progress
+class ProgressWorker(context: Context, parameters: WorkerParameters) :
+        CoroutineWorker(context, parameters) {
+    override suspend fun doWork(): Result {
+        setProgress(workDataOf(AppKeys.PROGRESS to 25))
+        //...
+        setProgress(workDataOf(AppKeys.PROGRESS to 50))
+        //...
+        return Result.success()
+    }
+}
+
+
 
 /*
 class MyApp: Application() {
@@ -120,3 +127,39 @@ class MyApp: Application() {
     }
 }
 */
+
+/*
+val uploadWorkRequest = OneTimeWorkRequestBuilder<UploadWorker>()
+        .setInitialDelay(10, TimeUnit.MINUTES)
+        .setBackoffCriteria(
+                BackoffPolicy.LINEAR,
+                OneTimeWorkRequest.MIN_BACKOFF_MILLIS,
+                TimeUnit.MILLISECONDS)
+        .setInputData(workDataOf(KEY_IMAGE_URI to imageUriString))
+        .setConstraints(constraints)
+        .addTag("cleanup")
+        .build()
+val uploadWorkRequest = PeriodicWorkRequestBuilder<UploadWorker>(1, TimeUnit.HOURS)
+        .setConstraints(constraints)
+        .build()
+
+ */
+
+/*
+WorkManager.getInstance(applicationContext).getWorkInfosByTag("Sync")
+WorkManager.getInstance(applicationContext).getWorkInfosForUniqueWork("MyUniqueName")
+WorkManager.getInstance(applicationContext).getWorkInfoById(uploadRequest.id)
+WorkManager.getInstance(applicationContext).getWorkInfosByTag(TAG_SAVE)
+WorkManager.getInstance(applicationContext).getWorkInfosByTagLiveData(TAG_SAVE)
+
+WorkManager.getInstance(applicationContext).cancelAllWork()
+*/
+
+//Source: developer.android.com/topic/libraries/architecture/workmanager/how-to/cancel-stop-work
+/*
+val saveImageWorkRequest = OneTimeWorkRequestBuilder<SaveImageWorker>()
+        .addTag(TAG_SAVE)
+        .build()
+
+WorkManager.getInstance(applicationContext).cancelWorkById(saveImageWorkRequest.id)
+WorkManager.getInstance(applicationContext).cancelAllWorkByTag(TAG_SAVE) */
